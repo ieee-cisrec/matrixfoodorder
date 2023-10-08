4c8ee97d9a07105c19fd2411dce496a6241aa26b
Sivabalan10 <135865311+Sivabalan10@users.noreply.github.com>
2023-10-08 21:59:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -20,6 +20,6 @@ Enjoy your convenient food ordering!<br><br>`;
         count-=1;
     }}
 function order(){
-    alert(`Please fill the form correctly! <br> Conformation message will be sent to your email <br> For any ordering issues only <br> Contact: +91 6383577376 , +91 9095779297 <br> [Not for any other purpose]`);
+    alert(`Please fill the form correctly! Conformation message will be sent to your email For any ordering issues only Contact: +91 6383577376 , +91 9095779297 [Not for any other purpose]`);
    
 }
