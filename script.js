7f5468c47c503e6ae8d4358bc8a840500d8a090d
Sivabalan10 <135865311+Sivabalan10@users.noreply.github.com>
2024-03-09 14:58:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+
+swal({title:"Maintenance",
+          text:`Due to scheduled maintenance, our website will be temporarily offline today. We apologize for any inconvenience this may cause. Normal service will resume next week. Thank you for your understanding and patience.`,       
+          });
+
 let count = 0;
 function guidelines(){
     let gui = document.getElementById('gui');
@@ -40,7 +45,7 @@ function order(link){
         window.location.href= b;
 }}
 
-const a = [1,1,0,1];
+const a = [0,0,0,0];
 // 0 - #Hotel_1, 1 - #Hotel_2, 2 - #Hotel_3,3 - #Hotel_4
 console.log("Automation - ON");
 setInterval(automate,1000);
